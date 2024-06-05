63a1f649ad22bcbe107b968c46e683d3716c955d
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-06-05 16:47:56 -0500

    Final PUSH

--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2683d47359f31dcd7717b3f132eb42f8d207de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
